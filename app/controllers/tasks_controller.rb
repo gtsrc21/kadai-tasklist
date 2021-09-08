c842f734fa2d7abacfe273c0b814845191ba2789
Ubuntu <ubuntu@ip-172-31-6-220.ap-northeast-1.compute.internal>
2021-09-08 13:31:38 +0000

    add login 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,14 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+
 
   def index
     if logged_in?
       # @tasks = Task.all
       @task = current_user.tasks.build  # form_with 用
       @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
-      
     else
       redirect_to login_path
     end
@@ -21,7 +23,6 @@ class TasksController < ApplicationController
   end
 
   def create
-    # @task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
 
     if @task.save
@@ -45,12 +46,14 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+     
 
   def destroy
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
+    
   end
   
   private
@@ -64,4 +67,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
 
-  resources :tasks
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-  resources :tasks, only: [:create, :update, :destroy]
+  resources :users, only: [:create]
+  resources :tasks, only: [:index, :show, :create, :new, :edit, :update, :destroy]
 end
